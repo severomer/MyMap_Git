db300ac92ca1c0f69017d1f1937d26a8eb452874
androcodeist <severomer86@gmail.com>
2019-05-14 14:04:43 +0300

    buttonlu için

--- app/src/main/java/com/example/mymap_git/MapsActivity.java
+++ app/src/main/java/com/example/mymap_git/MapsActivity.java
@@ -26,6 +26,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     //burayi ekledim
 
+
+    // burasi da buttonlu olsun
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
